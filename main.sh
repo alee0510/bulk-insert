e46e3a5b92bcc48ea3323edb0b34b0d84f9cf66c
alee0510 <ali.muksin0510@gmail.com>
2024-03-31 02:32:04 +0700

    chore: initialize code

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script is the main entry point for the application.
+# define the path to the script directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Script directory: $SCRIPT_DIR"
+
+
+
+
+
