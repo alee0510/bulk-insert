55fdee2a311c6b2cd4f81a624b0ed0c5d825108a
alee0510 <ali.muksin0510@gmail.com>
2024-04-05 05:33:35 +0700

    chore: add json convertion

--- main.sh
+++ main.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 # This script is the main entry point for the application.
 
-# read user input to get .csv data name in the current directory
-echo "Enter the .csv data file name in the current directory:"
-read -rp "> " DATA
-echo "Data: $DATA"
+# Read data from the file
+data="sample.csv"
 
-# check if the file exists
-if [ ! -f "$DATA" ]; then
-    echo "File not found!"
-    exit 1
+# clean the file and log if it's exist
+if [ -f json.txt ]; then
+    rm json.txt
 fi
 
-# read user input to get the API URL
-echo "Enter the API URL:"
-read -rp "> " API_URL
-echo "API URL: $API_URL"
+# Read the header line and split it into an array
+readarray -t header_array < <(head -n 1 "$data" | tr ',' '\n')
 
-# store the data in a variable
-DATA=$(cat "$DATA")
+# Loop through each line (starting from the second line) and process it
+tail -n +2 "$data" | while IFS=, read -r fields || [[ -n "$fields" ]]; do
+    echo "Processing line: $fields"
+    # Create JSON data using header as key and fields as value
+    # if value is empty, then it will be empty string
+    json="{"
+    for i in "${!header_array[@]}"; do
+        key=$(echo "${header_array[$i]}" | tr -d '\r')
+        value=$(echo "$fields" | cut -d',' -f$((i + 1)) | tr -d '\r')
+        json="$json\"$key\":\"$value\","
+    done
+    json="${json%,}"
+    json="$json}"
 
-# define funtion to convert each data row into JSON format
-function convert2json () {
-    # Get the header
-    header=$(echo "$DATA" | head -n 1)
-    echo "Header: $header"
+    # Print the JSON data
+    echo "$json"
 
-    # Get the data
-    data="$1"
-    echo "Data: $data"
-
-    # Combine the header and data
-}
-
-# Loop through the data and convert each row to JSON with the header as keys
-echo "$DATA" | tail -n +2 | while read -r line; do
-    # echo "Line: $line"
-
-    # Convert the line to JSON
-    convert2json "$line"
-
-    # Sleep for 1 second
-    sleep 1
+    # log the JSON data to a file
+    echo "$json" >> json.txt
 done
+
+# read the json file and print the data
+formatedData=$(cat json.txt)
+
+# check if the file is exist and not empty
+if [ -s json.txt ]; then
+    echo "$formatedData"
+else
+    echo "No data found"
+fi
